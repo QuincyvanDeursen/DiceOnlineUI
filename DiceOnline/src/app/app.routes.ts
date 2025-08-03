b9ece0e04e83d8e971064a7c62db48ea3bc64798
Quincy <Q.vandeursen@student.avans.nl>
2025-08-03 16:47:14 +0200

    try

--- DiceOnline/src/app/app.routes.ts
+++ DiceOnline/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
 
-export const routes: Routes = [];
+//add app component to index.html
+export const routes: Routes = [
+  { path: '', component: App }
+];
